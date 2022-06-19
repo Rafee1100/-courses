e61fd240ce8d60676753b572126f7165c8c8002b
Rafee1100 <rafee.abdullah1@gmail.com>
2022-06-19 08:11:59 +0600

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                &copy; <?php echo date('Y');?> || Abdullah Al Rafee</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
